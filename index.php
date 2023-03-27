6c56d67e0513588dce604920a96be485542d7db7
Borloo <supermaxime753@gmail.com>
2023-03-27 17:39:49 +0200

    Test

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8" />
 	<link rel="stylesheet" href="./include/styles.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<title>Mon site !</title>
 </head>
 <body>
@@ -14,9 +15,11 @@
 	<div class="wrapper">
 		<?php include("./include/menus.php"); ?>
 		<section id="content">
-		<?php
-			echo "<p>Bienvenue sur le site !</p>";
-		?>
+            <div class="card">
+                <div class="card-body">
+                    <p>Bienvenue sur le site !</p>
+                </div>
+            </div>
 		</section>
 	</div>
 	<?php include("./include/footer.php"); ?>
