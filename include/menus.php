89411b7fe08b211ffbcd6cc39308306c540c6a5e
Borloo <supermaxime753@gmail.com>
2023-03-27 23:55:46 +0200

    refacto

--- include/menus.php
+++ include/menus.php
@@ -1,16 +1,16 @@
 <nav class="sidebar">
   <ul>
-    <li><a href="index.php">Accueil</a></li>
-    <li><a href="ConsultDate.php">Consulter les emplacements par année de construction/rénovation</a></li>
+    <li><a href="./../index.php">Accueil</a></li>
+    <li><a href="./../app/ConsultDate.php">Consulter les emplacements par année de construction/rénovation</a></li>
 <?php
 		// si l'admin ne s'est pas déja connecté alors on affiche le lien pour cela
 		if (!isset($_SESSION['AdminConnecte'])) {
-			echo '<li><a href="./../src/Connexion.php">Se connecter comme admin </a></li>';
+			echo '<li><a href="../app/Connexion.php">Se connecter comme admin </a></li>';
 		}
 		// si l'admin est connecté alors on lui affiche des liens particuliers
 		else {
-			echo '<li><a href="ConsultType.php">Consulter les emplacements par type</a></li>';
-			echo '<li><a href="Deconnexion.php">Se déconnecter </a></li>';
+			echo '<li><a href="./../app/ConsultType.php">Consulter les emplacements par type</a></li>';
+			echo '<li><a href="./../app/Deconnexion.php">Se déconnecter </a></li>';
 		}
 ?>
   </ul>
