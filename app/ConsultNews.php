e847eb54f72aeac0449fd5e92372b090c172cc2f
blbcrv <clementpages02@gmail.com>
2023-03-28 12:25:38 +0200

    modifs consulter news

--- app/ConsultNews.php
+++ app/ConsultNews.php
@@ -10,7 +10,7 @@
 <body>
     <?php
     include("./include/header.php");
-    include("../include/connect.inc.php");
+    include("./include/connect.inc.php");
     ?>
     <div class="wrapper">
         <?php include("./include/menus.php"); ?>
