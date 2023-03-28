f1b3e7e740279b492acc4035fb107cbd3908f678
blbcrv <clementpages02@gmail.com>
2023-03-28 11:55:08 +0200

    modifs consulter news

--- app/ConsultNews.php
+++ app/ConsultNews.php
@@ -25,29 +25,42 @@
 
 
             try {
-                $reqnews = $conn->prepare("SELECT * FROM News");
+
+                $login = $_POST['login'];
+                $password = $_POST['password'];
+                $sql = "SELECT * FROM News";
+                $user = 'clmt';
+                $pass = '130702';
+                $conn = new PDO(
+                    'mysql:host=localhost;dbname=base_camping;charset=UTF8'
+                    ,
+                    $user,
+                    $pass,
+                    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+                );
+
+                $reqnews = $conn->prepare($sql);
                 $reqNews->execute();
                 $news = $reqnews->fetch();
+            
+                foreach ($news as $new) {
+                    echo "<tr>";
+                    echo "<td>" . $new["id"] . "</td>";
+                    echo "<td>" . $new["titre"] . "</td>";
+                    echo "<td>" . $new["body"] . "</td>";
+                    echo "</tr>";
+                }
                 $reqnews->closeCursor();
-                
-
-                // foreach ($news as $new) {
-                //     echo "<tr>";
-                //     echo "<td>" . $new["id"] . "</td>";
-                //     echo "<td>" . $new["titre"] . "</td>";
-                //     echo "<td>" . $new["body"] . "</td>";
-                //     echo "</tr>";
-                // }
-                
-            }catch(PDOException $e){
+            
+            } catch (PDOException $e) {
                 echo "Erreur : " . $e->getMessage();
             }
 
             echo "</table></center>";
             echo "<BR/><BR/>";
-            print_r($news)
+           
 
-            ?>
+                ?>
         </section>
     </div>
     <?php include("../include/footer.php"); ?>
