f3f95a4911db5f5eaac2f3148d99025527bef241
blbcrv <clementpages02@gmail.com>
2023-03-28 12:04:04 +0200

    modifs consulter news

--- app/ConsultNews.php
+++ app/ConsultNews.php
@@ -1,53 +1,69 @@
-<?php
-include("../include/header.php");
-include("../include/connect.inc.php");
+<!DOCTYPE html>
+<html>
 
-include("../include/menus.php");
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="../include/styles.css" />
+    <title>Mon site !</title>
+</head>
 
-/********************
-ConsultNews.php	
-*********************/
+<body>
+    <?php
+    include("../include/header.php");
+    include("../include/connect.inc.php");
+    ?>
+    <div class="wrapper">
+        <?php include("../include/menus.php"); ?>
+        <section id="content">
+            <?php
+            /********************
+            ConsultNews.php	
+            *********************/
 
-echo "<h1>les dernières news du camping</h1>";
-echo "<BR/><BR/>";
-
-
-try {
-
-    $login = $_POST['login'];
-    $password = $_POST['password'];
-    $sql = "SELECT * FROM News";
-    $user = 'clmt';
-    $pass = '130702';
-    $conn = new PDO(
-        'mysql:host=localhost;dbname=base_camping;charset=UTF8'
-        ,
-        $user,
-        $pass,
-        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
-    );
-
-    $reqnews = $conn->prepare($sql);
-    $reqNews->execute();
-    $news = $reqnews->fetch();
-
-    foreach ($news as $new) {
-        echo "<tr>";
-        echo "<td>" . $new["id"] . "</td>";
-        echo "<td>" . $new["titre"] . "</td>";
-        echo "<td>" . $new["body"] . "</td>";
-        echo "</tr>";
-    }
-    $reqnews->closeCursor();
-
-} catch (PDOException $e) {
-    echo "Erreur : " . $e->getMessage();
-}
-
-echo "</table></center>";
-echo "<BR/><BR/>";
+            echo "<h1>les dernières news du camping</h1>";
+            echo "<BR/><BR/>";
 
 
+            try {
 
-include("../include/footer.php");
-?>
+                $login = $_POST['login'];
+                $password = $_POST['password'];
+                $sql = "SELECT * FROM News";
+                $user = 'clmt';
+                $pass = '130702';
+                $conn = new PDO(
+                    'mysql:host=localhost;dbname=base_camping;charset=UTF8'
+                    ,
+                    $user,
+                    $pass,
+                    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+                );
+
+                $reqnews = $conn->prepare($sql);
+                $reqNews->execute();
+                $news = $reqnews->fetch();
+            
+                foreach ($news as $new) {
+                    echo "<tr>";
+                    echo "<td>" . $new["id"] . "</td>";
+                    echo "<td>" . $new["titre"] . "</td>";
+                    echo "<td>" . $new["body"] . "</td>";
+                    echo "</tr>";
+                }
+                $reqnews->closeCursor();
+            
+            } catch (PDOException $e) {
+                echo "Erreur : " . $e->getMessage();
+            }
+
+            echo "</table></center>";
+            echo "<BR/><BR/>";
+           
+
+                ?>
+        </section>
+    </div>
+    <?php include("../include/footer.php"); ?>
+</body>
+
+</html>
