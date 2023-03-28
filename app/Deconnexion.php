098f32c72293ed7bc2102acee68e8b0e6891efc6
Borloo <supermaxime753@gmail.com>
2023-03-28 19:55:18 +0200

    fix deco

--- app/Deconnexion.php
+++ app/Deconnexion.php
@@ -2,7 +2,6 @@
 	session_start();
 	require_once("./base/Functions.php");
 	unset($_SESSION['USER']);
-	deconnection();
 	// on aurait pu faire aussi : 	session_destroy();
 	// on redirige vers la page index.php
 	header('location: http://88.208.226.189/index.php');
