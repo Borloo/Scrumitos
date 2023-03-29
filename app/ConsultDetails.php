1c52b629f05987a0129ac23dadf8048001576311
Svlmon <foras.titouan@gmail.com>
2023-03-29 16:21:52 +0200

    Changement PageDetails et Recherche par details

--- app/ConsultDetails.php
+++ app/ConsultDetails.php
@@ -1,18 +1,34 @@
+<?php
+	session_start();
+	// si l'internaute accède à cette page sans être l'admin connecté alors
+	// on le renvoie vers la page indexphp
+	if (!isset($_SESSION['USER'])) {
+		header('location: index.php');
+		die();	
+	}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8" />
-	<link rel="stylesheet" href="../include/styles.css" />
+	<link rel="stylesheet" href="./include/styles.css" />
 	<title>Mon site !</title>
 </head>
 <body>
 	<?php 
-		session_start();
-		include("../include/header.php"); 
+		include("./include/header.php"); 
+		include("./include/connect.inc.php");
 	?>
 	<div class="wrapper">
-		<?php include("../include/menus.php"); ?>
-		
-	<?php include("../include/footer.php"); ?>
+		<?php include("./include/menus.php"); ?>
+		<section id="content">
+		<form method="get" action="rechercheDetails.php">
+  			<input type="text" name="search" placeholder="Rechercher...">
+  			<button type="submit">Rechercher</button>
+		</form>
+		</section>
+	</div>
+	<?php include("./include/footer.php"); ?>
 </body>
 </html>
