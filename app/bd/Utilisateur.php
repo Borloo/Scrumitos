c9a4453519c1705b21a056b4019dd5b3b7fb3708
Borloo <supermaxime753@gmail.com>
2023-03-29 14:15:47 +0200

    refacto code

--- app/bd/Utilisateur.php
+++ app/bd/Utilisateur.php
@@ -1,19 +1,6 @@
 <?php
 
-    function getConnexion(): PDO
-    {
-        try{
-            $user = 'clmt';
-            $pass = '130702';
-            $conn = new PDO('mysql:host=localhost;dbname=base_camping;charset=UTF8'
-                ,$user, $pass, array(PDO::ATTR_ERRMODE =>PDO::ERRMODE_EXCEPTION));
-        }
-        catch (PDOException $e){
-            echo "Erreur: ".$e->getMessage()."<br/>";
-            die() ;
-        }
-        return $conn;
-    }
+    require('./base/Functions.php');
 
     function getUser(string $login, string $password)
     {
