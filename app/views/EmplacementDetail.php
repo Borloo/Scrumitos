8b038876d74fd7b9851cec4f5398dff09322da91
Borloo <supermaxime753@gmail.com>
2023-03-29 19:08:25 +0200

    test mapping data

--- app/views/EmplacementDetail.php
+++ app/views/EmplacementDetail.php
@@ -23,7 +23,25 @@
 
                         $emplacement = getOneEmplacementById((int)$_GET['id']);
                         if (null != $emplacement){
+                            $name = $emplacement['Nom_Emplacement'];
+                            $id = $emplacement['idEmpl'];
+                            $typeId = $emplacement['idType'];
                             print_r($emplacement);
+                            echo "
+                                <div class='card-headear'>
+                                    <h1>" . $id . " - " . $name . "</h1>
+                                </div>
+                                <div class='card-body'>
+                                    <div class='row'>
+                                        <div class='col-md-6'>
+                                            <input name='name' type='text' value='" . $name . "'>
+                                        </div>
+                                        <div class='col-md-6'>
+                                            <input name='name' type='text' value='" . $typeId . "'>
+                                        </div>
+                                    </div>
+                                </div>
+                            ";
                         }else{
                             echo "<p>Il semblerait il y avoir un problème, cliquez <a href='./../../index.php'>ici</a> pour retourner à l'acceuil</p>";
                         }
