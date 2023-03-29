235da165ef03988e774393e7bf5483e9f9d3a443
Borloo <supermaxime753@gmail.com>
2023-03-30 01:02:29 +0200

    refacto

--- app/views/EmplacementDetail.php
+++ app/views/EmplacementDetail.php
@@ -36,19 +36,19 @@
                                 $dateDeb = $dateDeb->format('d/m/Y H:i');
                                 print_r($dateDeb);
                             }else{
+                                $name = $emplacement['Nom_Emplacement'];
+                                $id = $emplacement['idEmpl'];
+                                $typeId = $emplacement['idType'];
+                                $adresse = $emplacement['adresseEmpl'];
+                                $annee = $emplacement['anneeConstruction'];
+                                $taille = $emplacement['Taille'];
+                                $maxPersonne = $emplacement['Max_Personnes'];
+                                $dateFin = $emplacement['Periode_Dispo_Fin'];
+                                $prixSemaine = $emplacement['Prix_Semaine'];
+                                $prixAnnee = $emplacement['Prix_Periode_Annee'];
+                                $options = $emplacement['Options'];
                                 $dateDeb = $emplacement['Periode_Dispo_Debut'];
                             }
-                            $name = $emplacement['Nom_Emplacement'];
-                            $id = $emplacement['idEmpl'];
-                            $typeId = $emplacement['idType'];
-                            $adresse = $emplacement['adresseEmpl'];
-                            $annee = $emplacement['anneeConstruction'];
-                            $taille = $emplacement['Taille'];
-                            $maxPersonne = $emplacement['Max_Personnes'];
-                            $dateFin = $emplacement['Periode_Dispo_Fin'];
-                            $prixSemaine = $emplacement['Prix_Semaine'];
-                            $prixAnnee = $emplacement['Prix_Periode_Annee'];
-                            $options = $emplacement['Options'];
                             if ($_GET['maj'] == 1){
                                 echo "<h4>Emplacement mise à jour</h4><br/>";
                             }
