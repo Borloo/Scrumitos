ae74904078e9e2844ac4c91037043ad5486c5e23
Borloo <supermaxime753@gmail.com>
2023-03-29 16:11:15 +0200

    test

--- app/views/ConsultType.php
+++ app/views/ConsultType.php
@@ -26,56 +26,67 @@
 	<div class="wrapper">
 		<?php include("./../../include/menus.php"); ?>
 		<section id="content">
-		<?php
-			/********************
-				ConsultType.php	
-			*********************/
-				// le formulaire de saisie du type d'emplacement recherché
-				echo "<h1>Consulter les emplacements par type </h1>";
-				echo "<BR/><BR/>";
-				echo "<form method='post'>";
-					echo "<fieldset>";
-						echo "<legend> Types d'Emplacement </legend><BR/>";	
-						// LD_Types = Liste Déroulante des types
-						echo "<select name='LD_Types'>";
-						// on constitue la liste déroulante à partir de la table Type 
-						$reqType = $conn->prepare("SELECT * FROM Type");
-						$reqType->execute();	
-						foreach($reqType as $type) {
-							echo "<option value='".$type["idType"]."'>".$type["nomType"]."</option>";
-						}	
-						$reqType->closeCursor();		
-						echo "</select><br/><br/>";			
-						echo "<input type='submit' name='Afficher' value='Afficher'/>";
-						echo "<br/><br/>";
-					echo "</fieldset>";
-				echo "</form>";		
-				
-			
-				// le formulaire a été soumis
-				if(isset($_POST['Afficher']) && isset($_POST['LD_Types'])) {
-					// echo $_POST['LD_Types'];					
-						
-					// on affiche le tableau des résultats
-					echo "<BR/><BR/>";
-					echo "<center><table border='2' >";
-						echo "<caption> Emplacement du Type ".$_POST['LD_Types']."</caption>";
-						echo "<tr><th>Id Emp</th><th>Type de l'Emplacement</th><th>Adresse Emplacement</th><th>Année de Construction</th></tr>";	
-						$reqEmpl = $conn->prepare("SELECT * FROM Emplacement where idType = :pIdType");
-						$reqEmpl->execute(array('pIdType' => $_POST['LD_Types']));
-						foreach($reqEmpl as $empl) {
-							echo "<tr>";
-								echo "<td>".$empl["idEmpl"]."</td>";
-								echo "<td>".$empl["idType"]."</td>";
-								echo "<td>".$empl["adresseEmpl"]."</td>";
-								echo "<td>".$empl["anneeConstruction"]."</td>";
-							echo "</tr>";	
-						}	
-						$reqEmpl->closeCursor();
-					echo "</table></center>";	
-					echo "<BR/><BR/>";
-				}		
-		?>
+            <div class="card">
+                <div class="card-headear">
+                    <h1>Consulter les emplacements par type </h1>
+                </div>
+                <div class="card-body">
+                    <?php
+                        require('./../base/Functions.php');
+                        echo "ok";
+                    ?>
+                </div>
+            </div>
+<!--		--><?php
+//			/********************
+//				ConsultType.php
+//			*********************/
+//				// le formulaire de saisie du type d'emplacement recherché
+//				echo "";
+//				echo "<BR/><BR/>";
+//				echo "<form method='post'>";
+//					echo "<fieldset>";
+//						echo "<legend> Types d'Emplacement </legend><BR/>";
+//						// LD_Types = Liste Déroulante des types
+//						echo "<select name='LD_Types'>";
+//						// on constitue la liste déroulante à partir de la table Type
+//						$reqType = $conn->prepare("SELECT * FROM Type");
+//						$reqType->execute();
+//						foreach($reqType as $type) {
+//							echo "<option value='".$type["idType"]."'>".$type["nomType"]."</option>";
+//						}
+//						$reqType->closeCursor();
+//						echo "</select><br/><br/>";
+//						echo "<input type='submit' name='Afficher' value='Afficher'/>";
+//						echo "<br/><br/>";
+//					echo "</fieldset>";
+//				echo "</form>";
+//
+//
+//				// le formulaire a été soumis
+//				if(isset($_POST['Afficher']) && isset($_POST['LD_Types'])) {
+//					// echo $_POST['LD_Types'];
+//
+//					// on affiche le tableau des résultats
+//					echo "<BR/><BR/>";
+//					echo "<center><table border='2' >";
+//						echo "<caption> Emplacement du Type ".$_POST['LD_Types']."</caption>";
+//						echo "<tr><th>Id Emp</th><th>Type de l'Emplacement</th><th>Adresse Emplacement</th><th>Année de Construction</th></tr>";
+//						$reqEmpl = $conn->prepare("SELECT * FROM Emplacement where idType = :pIdType");
+//						$reqEmpl->execute(array('pIdType' => $_POST['LD_Types']));
+//						foreach($reqEmpl as $empl) {
+//							echo "<tr>";
+//								echo "<td>".$empl["idEmpl"]."</td>";
+//								echo "<td>".$empl["idType"]."</td>";
+//								echo "<td>".$empl["adresseEmpl"]."</td>";
+//								echo "<td>".$empl["anneeConstruction"]."</td>";
+//							echo "</tr>";
+//						}
+//						$reqEmpl->closeCursor();
+//					echo "</table></center>";
+//					echo "<BR/><BR/>";
+//				}
+//		?>
 		</section>
 	</div>
 	<?php include("./../../include/footer.php"); ?>
