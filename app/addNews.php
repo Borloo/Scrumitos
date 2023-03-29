483fa11081f2c383d5e8f856d739ab8e2ed6603e
blbcrv <clementpages02@gmail.com>
2023-03-29 16:06:19 +0200

    modif encore

--- app/addNews.php
+++ app/addNews.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 'on');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href=".../include/styles.css" />
+    <link rel="stylesheet" href="../include/styles.css" />
     <title>Ajout de news</title>
 </head>
 <body>
