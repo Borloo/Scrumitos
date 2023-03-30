0d29df4590ef0ef05e39f67af6f105cdcc203ed9
blbcrv <clementpages02@gmail.com>
2023-03-30 15:37:52 +0200

    aller hop on fait le menage dans le menu

--- include/menus.php
+++ include/menus.php
@@ -1,7 +1,6 @@
 <nav class="sidebar">
   <ul>
 	<li><a href="/app/ConsultNews.php">News</a></li>
-    <li><a href="/app/ConsultDate.php">Emplacements par année</a></li>
 	<li><a href="/app/ConsultEmplacementPrix.php">Emplacement par prix</a></li>
     <li><a href="/app/ConsultEmpAvance.php">Consultation avancée des emplacements</a></li>
 <?php
@@ -12,7 +11,6 @@
 		// si l'admin est connecté alors on lui affiche des liens particuliers
 		else {
 			echo '<li><a href="/app/views/ConsultEmplacement.php">Recherche emplacements</a></li>';
-            echo '<li><a href="/app/views/ConsultType.php?suppr=0&add=0">Emplacements par type</a></li>';
 			echo '<li><a href="/app/addNews.php">Ajouter des news</a></li>';
 			echo '<li><a href="/app/Deconnexion.php">Se déconnecter </a></li>';
 		}
