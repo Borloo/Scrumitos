0e8576dc58ebb632cf05dc93fb2bf4735e1ecc1a
JulienOusmer <julienousmer46090@gmail.com>
2023-03-30 17:42:16 +0200

    fix menu

--- include/menus.php
+++ include/menus.php
@@ -1,26 +1,17 @@
 <nav class="sidebar">
-  <ul>
-	<li><a href="/app/ConsultNews.php">News</a></li>
-	<li><a href="/app/ConsultEmplacementPrix.php">Emplacement par prix</a></li>
-
-    <li><a href="/app/ConsultEmpAvance.php">Consultation avancée des emplacements</a></li>
-    <li><a href="/index.php">Accueil</a></li>
-	<li><a href="/app/ConsultNews.php">Consulter les news</a></li>
-    <li><a href="/app/ConsultDate.php">Consulter les emplacements par année de construction/rénovation</a></li>
-	<li><a href="/app/ConsultEmplacementPrix.php">Consultation des emplacements par prix</a></li>
-	<li><a href="/app/ConsultDetails.php">Consulter les détails d'un emplacement</a></li>
-	<li><a href="/app/ConsultDetails.php">Les emplacements</a></li>
-<?php
-		// si l'admin ne s'est pas déja connecté alors on affiche le lien pour cela
-		if (!isset($_SESSION['USER'])) {
-			echo '<li><a href="/app/Connexion.php">Se connecter comme admin </a></li>';
-		}
-		// si l'admin est connecté alors on lui affiche des liens particuliers
-		else {
-			echo '<li><a href="/app/views/ConsultEmplacement.php">Recherche emplacements</a></li>';
-			echo '<li><a href="/app/addNews.php">Ajouter des news</a></li>';
-			echo '<li><a href="/app/Deconnexion.php">Se déconnecter </a></li>';
-		}
-?>
-  </ul>
+    <ul>
+        <li><a href="/index.php">Accueil</a></li>
+        <li><a href="/app/ConsultNews.php">News</a></li>
+        <li><a href="/app/views/ConsultEmplacement.php">Recherches d'emplacements</a></li>
+        <?php
+        // si l'admin ne s'est pas déja connecté alors on affiche le lien pour cela
+        if (!isset($_SESSION['USER'])) {
+            echo '<li><a href="/app/Connexion.php">Se connecter comme admin </a></li>';
+        } // si l'admin est connecté alors on lui affiche des liens particuliers
+        else {
+            echo '<li><a href="/app/addNews.php">Ajouter des news</a></li>';
+            echo '<li><a href="/app/Deconnexion.php">Se déconnecter </a></li>';
+        }
+        ?>
+    </ul>
 </nav>
