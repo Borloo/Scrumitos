e3ba5ac2921de659b4ba2e6f1a701974e9d89caf
JulienOusmer <julienousmer46090@gmail.com>
2023-03-30 17:47:03 +0200

    fix menu sur toutes les pages normalement

--- app/ConsultNews.php
+++ app/ConsultNews.php
@@ -1,79 +1,84 @@
-<?php 
+<?php
+session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
+if (!isset($_SESSION['USER'])) {
+    header('location: index.php');
+    die();
+}
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" href="../include/styles.css" />
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="../include/styles.css"/>
     <title>Mon site !</title>
     <?php
-        include("../include/headfile.php");
+    include("../include/headfile.php");
     ?>
 </head>
 
 <body>
-    <?php
-    include("../include/header.php");
-    // include("../include/connect.inc.php");
-    ?>
-    <div class="wrapper">
-        <?php include("../include/menus.php"); ?>
-        <section id="content">
-            <div class="card">
-                <div class="card-body">
-                    <?php
-                    /********************
-                    ConsultNews.php	
-                    *********************/
+<?php
+include("../include/header.php");
+// include("../include/connect.inc.php");
+?>
+<div class="wrapper">
+    <?php include("../include/menus.php"); ?>
+    <section id="content">
+        <div class="card">
+            <div class="card-body">
+                <?php
+                /********************
+                 * ConsultNews.php
+                 *********************/
 
-                    echo "<h1>les dernières news du camping</h1>";
-                    echo "<BR/><BR/>";
+                echo "<h1>les dernières news du camping</h1>";
+                echo "<BR/><BR/>";
 
 
-                    try {
+                try {
 
-                        $sql = "SELECT titre, body, date FROM News order by date desc";
-                        $user = 'clmt';
-                        $pass = '130702';
-                        $conn = new PDO(
-                            'mysql:host=localhost;dbname=base_camping;charset=UTF8'
-                            ,
-                            $user,
-                            $pass,
-                            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
-                        );
+                    $sql = "SELECT titre, body, date FROM News order by date desc";
+                    $user = 'clmt';
+                    $pass = '130702';
+                    $conn = new PDO(
+                        'mysql:host=localhost;dbname=base_camping;charset=UTF8'
+                        ,
+                        $user,
+                        $pass,
+                        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+                    );
 
-                        $reqnews = $conn->prepare($sql);
-                        $reqnews->execute();
-                        $news = $reqnews->fetchAll();
+                    $reqnews = $conn->prepare($sql);
+                    $reqnews->execute();
+                    $news = $reqnews->fetchAll();
 
-                        foreach ($news as $new) {
-                            echo "<tr>";
-                            echo "<td>" . $new['date'] . " - </td>";
-                            echo "<td>" . $new['titre'] . "</td><br><br>";
-                            echo "<td>" . $new['body'] . "</td><br>";
-                            echo "</tr><br><br>";
-                        }
-                        $reqnews->closeCursor();
-
-                    } catch (PDOException $e) {
-                        echo "Erreur : " . $e->getMessage();
+                    foreach ($news as $new) {
+                        echo "<tr>";
+                        echo "<td>" . $new['date'] . " - </td>";
+                        echo "<td>" . $new['titre'] . "</td><br><br>";
+                        echo "<td>" . $new['body'] . "</td><br>";
+                        echo "</tr><br><br>";
                     }
+                    $reqnews->closeCursor();
 
-                    echo "</table></center>";
-                    echo "<BR/><BR/>";
+                } catch (PDOException $e) {
+                    echo "Erreur : " . $e->getMessage();
+                }
+
+                echo "</table></center>";
+                echo "<BR/><BR/>";
 
 
-                    ?>
-                </div>
+                ?>
             </div>
-        </section>
-    </div>
-    <?php include("../include/footer.php"); ?>
+        </div>
+    </section>
+</div>
+<?php include("../include/footer.php"); ?>
 </body>
 
 </html>
