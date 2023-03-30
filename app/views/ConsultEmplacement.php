f8dc748162cf3e08823e383a98ef9e5a7ba7ee9c
JulienOusmer <julienousmer46090@gmail.com>
2023-03-30 17:49:35 +0200

    fix perm

--- app/views/ConsultEmplacement.php
+++ app/views/ConsultEmplacement.php
@@ -2,11 +2,6 @@
 session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
-if (!isset($_SESSION['USER'])) {
-    header('location: index.php');
-    die();
-}
-
 ?>
 <!DOCTYPE html>
 <html>
