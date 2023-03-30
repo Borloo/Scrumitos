5b9a695e5241748ccff73260ac179b7d107ab4f7
JulienOusmer <julienousmer46090@gmail.com>
2023-03-30 17:35:39 +0200

    Test menu fix

--- app/addNews.php
+++ app/addNews.php
@@ -1,7 +1,14 @@
-<?php 
+<?php
+session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
-?><!DOCTYPE html>
+if (!isset($_SESSION['USER'])) {
+    header('location: index.php');
+    die();
+}
+
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
