361fdc1c283a2a9f6a1370a10da93d9a48e1aad9
blbcrv <clementpages02@gmail.com>
2023-03-30 14:53:12 +0200

    debut refacto

---
+++ app/views/ConsultEmplacement.php
@@ -0,0 +1,77 @@
+<?php
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+if (!isset($_SESSION['USER'])) {
+    header('location: index.php');
+    die();
+}
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php
+    include("./../../include/headfile.php");
+    ?>
+    <style>
+        td {
+            text-align: center;
+            padding: 5px;
+        }
+
+        th {
+            padding: 5px 20px 5px 20px;
+        }
+
+        table {
+            border: 1px solid black;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    include("./../../include/header.php");
+    ?>
+    <div class="wrapper">
+        <?php include("./../../include/menus.php"); ?>
+        <section id="content">
+            <div class="card">
+                <div class="card-headear">
+                    <h1>Recherche d'emplacements </h1>
+                </div>
+            <?php
+			echo "<BR/><BR/>";
+			echo "<form method='post'>";
+			echo "<fieldset>";
+			echo "<legend> Emplacements </legend><BR/>";
+			// BR_Choix = Bouton Radio du choix
+			echo "<input type='radio' name='BR_choix' value='byType' checked='checked'";
+			// on garde la sélection effectuée précédemment
+			if (isset($_POST['Afficher']) && isset($_POST['BR_choix']) && $_POST['BR_choix'] == "byType") {
+				echo "checked='checked'";
+			}
+			echo "/> Recherche d'emplacements par type<BR/><BR/>";
+			echo "<input type='radio' name='BR_choix' value='byPeriod' ";
+			if (isset($_POST['Afficher']) && isset($_POST['BR_choix']) && $_POST['BR_choix'] == "byPeriod") {
+				echo "checked='checked'";
+			}
+			echo "/> Recherche d'emplacements par période<BR/><BR/>";
+			echo "<input type='radio' name='BR_choix' value='bySize' ";
+			if (isset($_POST['Afficher']) && isset($_POST['BR_choix']) && $_POST['BR_choix'] == "bySize") {
+				echo "checked='checked'";
+			}
+			echo "/> Recherche d'emplacement par taille<BR/><BR/>";
+			echo "<input type='submit' name='Afficher' value='Afficher'/><BR/><BR/>";
+			echo "</fieldset>";
+			echo "</form>";
+                
+        ?>
+        </section>
+    </div>
+    <?php include("./../../include/footer.php"); ?>
+</body>
+
+</html>
