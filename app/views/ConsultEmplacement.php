892f9da867511c0c0944781ef9d3989f67706965
JulienOusmer <julienousmer46090@gmail.com>
2023-03-30 18:10:14 +0200

    la c good les redirects

--- app/views/ConsultEmplacement.php
+++ app/views/ConsultEmplacement.php
@@ -43,68 +43,8 @@ ini_set('display_errors', 'on');
                     <a class="btn btn-secondary" href="" role="button">Recherche emplacement par période</a>
                     <a class="btn btn-secondary" href="../ConsultEmplacementPrix.php" role="button">consulter emplacement année construction</a>
                     <a class="btn btn-secondary" href="../ConsultEmplacementPrix.php" role="button">Recherche emplacement prix</a>
-            <?php
-			echo "<BR/><BR/>";
-			echo "<form method='post'>";
-			echo "<fieldset>";
-			// BR_Choix = Bouton Radio du choix
-			echo "<input type='radio' name='BR_choix' value='byType' checked='checked'";
-			// on garde la sélection effectuée précédemment
-			if (isset($_POST['Afficher']) && isset($_POST['BR_choix']) && $_POST['BR_choix'] == "byType") {
-				echo "checked='checked'";
-			}
-			echo "/> Recherche d'emplacements par type<BR/><BR/>";
-			echo "<input type='radio' name='BR_choix' value='byType' ";
-			if (isset($_POST['Afficher']) && isset($_POST['BR_choix']) && $_POST['BR_choix'] == "byPeriod") {
-				echo "checked='checked'";
-			}
-			echo "/> Recherche d'emplacements par période<BR/><BR/>";
-            echo "<input type='radio' name='BR_choix' value='byYear' ";
-            if (isset($_POST['Afficher']) && isset($_POST['BR_choix']) && $_POST['BR_choix'] == "byYear") {
-                echo "checked='checked'";
-            }
-            echo "/> Recherche d'emplacements par années de construction<BR/><BR/>";
-			echo "<input type='radio' name='BR_choix' value='byPrice' ";
-			if (isset($_POST['Afficher']) && isset($_POST['BR_choix']) && $_POST['BR_choix'] == "bySize") {
-				echo "checked='checked'";
-			}
-            echo "/> Recherche d'emplacements par prix<BR/><BR/>";
-            echo "<input type='radio' name='BR_choix' value='bySize' ";
-            if (isset($_POST['Afficher']) && isset($_POST['BR_choix']) && $_POST['BR_choix'] == "byPrice") {
-                echo "checked='checked'";
-            }
-			echo "/> Recherche d'emplacement par taille<BR/><BR/>";
-			echo "<input type='submit' name='Afficher' value='Afficher'/><BR/><BR/>";
-			echo "</fieldset>";
-			echo "</form>";
-
-            // le formulaire a été soumis
-			if (isset($_POST['Afficher']) && isset($_POST['BR_choix'])) {
-				// echo $_POST['BR_choix'];	
-				//  on sélectionne les emplacements recherchés					
-				switch ($_POST['BR_choix']) {
-					case "byType":
-                        header('location: ConsultType.php?suppr=0&add=0');
-						break;
-
-                    case "byYear" :
-                        header('location: /app/ConsultDate.php');
-                        break;
-
-					case "byPeriod":
-						$titre = "Emplacements par périodes";
-						break;
-                    case "byPrice":
-                        header('location: /app/ConsultEmplacementPrix.php');
-                        $titre = "Emplacements par prix";
-                        break;
-                    case "bySize":
-						$titre = "Emplacements par taille";
-						break;
-                }
-            }
-                
-        ?>
+                    <a class="btn btn-secondary" href="p" role="button">Recherche emplacement taille</a>
+           
                 </div>
             </div>
         </section>
