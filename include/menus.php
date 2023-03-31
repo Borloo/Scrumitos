3811a82cdcdff0df154cca804704cc787d8bc368
JulienOusmer <julienousmer46090@gmail.com>
2023-03-31 08:49:42 +0200

    ok fail

--- include/menus.php
+++ include/menus.php
@@ -3,7 +3,6 @@
         <li class="list-group-item"><a href="/index.php">Accueil</a></li>
         <li class="list-group-item"><a href="/app/ConsultNews.php">News</a></li>
         <li class="list-group-item"><a href="/app/views/ConsultEmplacement.php?c=0">Recherches d'emplacements</a></li>
-        <li class="list-group-item"><a href="/app/views/EmplacementDetail.php">Détail d'un emplacement</a></li>
         <?php
         // si l'admin ne s'est pas déja connecté alors on affiche le lien pour cela
         if (!isset($_SESSION['USER'])) {
