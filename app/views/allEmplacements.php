0b131821e07fcacf65f3ad51fc43d41c4be12202
JulienOusmer <julienousmer46090@gmail.com>
2023-03-31 08:57:53 +0200

    appel function test

--- app/views/allEmplacements.php
+++ app/views/allEmplacements.php
@@ -2,4 +2,23 @@
 session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
-getHtmlPeriode();
+include("./../../include/headfile.php");
+require('./../base/HtmlFunctions.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include("./../../include/headfile.php"); ?>
+</head>
+<body>
+<?php include("./../../include/header.php"); ?>
+<div class="wrapper">
+    <?php
+    include("./../../include/menus.php");
+    getHtmlPeriode();
+    ?>
+</div>
+<?php include("./../../include/footer.php"); ?>
+</body>
+</html>
