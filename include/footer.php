23dc607f8b14e69d250fc477d4a987a062b8a11c
JulienOusmer <julienousmer46090@gmail.com>
2023-03-31 12:11:33 +0200

    fix footer final

--- include/footer.php
+++ include/footer.php
@@ -1,10 +1,5 @@
 <footer class="globalFooter py-3 my-4" id="bottom">
-    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <li class="nav-item"><a href="../index.php" class="nav-link px-2 text-muted">Accueil</a></li>
-        <li class="nav-item"><a href="./app/views/ConsultNews.php" class="nav-link px-2 text-muted">News</a></li>
-        <li class="nav-item"><a href="./app/views/ConsultEmplacement.php" class="nav-link px-2 text-muted">Recherches</a></li>
-        <li class="nav-item"><a href="./app/views/allEmplacements.php" class="nav-link px-2 text-muted">Consultation</a></li>
-    </ul>
+    <p><strong>Membres :</strong>Noémie - Titouan - Clément - Maxime - Thibaut - Julien</p>
   <p class="text-center text-muted">© 2023 LP APSIO, Groupe 2</p>
 </footer>
 
