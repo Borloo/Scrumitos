89f133152ad1c5251fb692c26139231481603532
Borloo <supermaxime753@gmail.com>
2023-03-31 16:22:34 +0200

    try redirect

--- app/base/HtmlFunctions.php
+++ app/base/HtmlFunctions.php
@@ -535,7 +535,7 @@ function getHtmlEmplacementTable(array $emplacements, string $specify = '')
                         <a href='./EmplacementDetail.php?maj=0&id=" . $emplacement['idEmpl'] . "&edit=1'><input type='button' class='btn btn-warning' value='Modifier'></a>
                     </div>
                     <div class='col-md-4'>
-                        <a href='./EmplacementDetail.php?maj=0&id=" . $emplacement['idEmpl'] . "&edit=0'><input type='button' class='btn btn-info' value='Prévisualiser'></a>
+                        <a href='./EmplacementDetail.php?maj=0&id=" . $emplacement['idEmpl'] . "&edit=0'><input type='button' class='btn btn-info' value='Détails'></a>
                     </div>
                     <div class='col-md-4'>
                         <a href='./EmplacementDetail.php?maj=0&id=" . $emplacement['idEmpl'] . "&edit=3'><input type='button' class='btn btn-danger' value='Supprimer'></a>
--- app/views/NewsDetail.php
+++ app/views/NewsDetail.php
@@ -1,3 +1,57 @@
 <?php
-
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include("./../../include/headfile.php"); ?>
+    <style>
+        td {
+            text-align: center;
+            padding: 5px;
+        }
+
+        th {
+            padding: 5px 20px 5px 20px;
+        }
+
+        table {
+            border: 1px solid black;
+        }
+
+        select {
+            width: 100%;
+        }
+
+        .btn {
+            width: 100%;
+        }
+
+        .row {
+            margin: 2%;
+        }
+
+        .card {
+            margin-bottom: 2%;
+            height: 100%;
+        }
+    </style>
+</head>
+
+<body>
+<?php include("./../../include/header.php"); ?>
+    <div class="wrapper">
+        <?php include("./../../include/menus.php");?>
+        <section id="content">
+            <div class="card">
+                <p>ok</p>
+            </div>
+        </section>
+    </div>
+</body>
+<?php include("../../include/footer.php"); ?>
+</html>
