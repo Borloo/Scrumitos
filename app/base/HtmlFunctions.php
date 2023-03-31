1dfde580ffcd512f87e57a6f40e4fc815001c8b0
JulienOusmer <julienousmer46090@gmail.com>
2023-03-31 10:01:57 +0200

    c bon ca fonctionne

--- app/base/HtmlFunctions.php
+++ app/base/HtmlFunctions.php
@@ -135,7 +135,7 @@ function getHtmlType()
                                 <div class='col-md-4'>
                                     <input class='btn btn-secondary' type='submit' id='submit' name='submit' value='Afficher'>
                                 </div>";
-                                if (!isset($_SESSION['USER'])) {
+                                if (isset($_SESSION['USER'])) {
                                     echo "
                                     <div class='col-md-4'>
                                         <a href='./EmplacementDetail.php?maj=0&id=-1&edit=2'><input class='btn btn-info' id='ajouter' type='button' value='Ajouter'></a>
