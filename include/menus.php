953732177e41b3475dd1595e4efa29d7f050891b
JulienOusmer <julienousmer46090@gmail.com>
2023-03-31 15:26:13 +0200

    hop text deco

--- include/menus.php
+++ include/menus.php
@@ -1,9 +1,9 @@
 <nav class="sidebar">
     <ul class="list-group">
-        <a href="/index.php"><li class="list-group-item">Accueil</li></a>
-        <li class="list-group-item"><a href="/app/views/ConsultNews.php">News</a></li>
-        <li class="list-group-item"><a href="/app/views/ConsultEmplacement.php?c=0">Recherches d'emplacements</a></li>
-        <li class="list-group-item"><a href="/app/views/allEmplacements.php">Consulter tous les emplacements</a></li>
+        <li class="list-group-item"><a href="/index.php" class="text-decoration-none">Accueil</a></li>
+        <li class="list-group-item"><a href="/app/views/ConsultNews.php" class="text-decoration-none">News</a></li>
+        <li class="list-group-item"><a href="/app/views/ConsultEmplacement.php?c=0" class="text-decoration-none">Recherches d'emplacements</a></li>
+        <li class="list-group-item"><a href="/app/views/allEmplacements.php" class="text-decoration-none">Consulter tous les emplacements</a></li>
         <?php
         // si l'admin ne s'est pas déja connecté alors on affiche le lien pour cela
         if (!isset($_SESSION['USER'])) {
