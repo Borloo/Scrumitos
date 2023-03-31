88d4a80ac65595643a77dfbf948a3f9efa9c1bdc
JulienOusmer <julienousmer46090@gmail.com>
2023-03-31 11:57:32 +0200

    footer ltsgo

--- include/footer.php
+++ include/footer.php
@@ -1,3 +1,4 @@
 <footer class="globalFooter" id="bottom">
-  <p>Contact : maximetcheverria@gmail.com </p>
+  <p>LP APSIO - Groupe 2 </p>
+    <p><strong>Mmebres :</strong> Maxime, Titouan, Clément, Noémie, Thibaut, Julien</p>
 </footer>
