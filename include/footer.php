2416222356b142437e00fd61674b2d12fd0939b6
JulienOusmer <julienousmer46090@gmail.com>
2023-03-31 11:58:28 +0200

    add copyright bb

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,4 @@
 <footer class="globalFooter" id="bottom">
-  <p>LP APSIO - Groupe 2 </p>
-    <p><strong>Mmebres :</strong> Maxime, Titouan, Clément, Noémie, Thibaut, Julien</p>
+  <p>LP APSIO - Groupe 2 ©</p>
+    <p><strong>Membres :</strong> Maxime, Titouan, Clément, Noémie, Thibaut, Julien</p>
 </footer>
