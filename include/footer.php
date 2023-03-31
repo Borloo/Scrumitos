09154a6a4fe7cc133eead45c45f7243e9c47f212
JulienOusmer <julienousmer46090@gmail.com>
2023-03-31 12:12:10 +0200

    dsl oublier class

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,5 @@
 <footer class="globalFooter py-3 my-4" id="bottom">
-    <p><strong>Membres :</strong>Noémie - Titouan - Clément - Maxime - Thibaut - Julien</p>
+    <p class="text-center text-muted"><strong>Membres :</strong>Noémie - Titouan - Clément - Maxime - Thibaut - Julien</p>
   <p class="text-center text-muted">© 2023 LP APSIO, Groupe 2</p>
 </footer>
 
