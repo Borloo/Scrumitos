e72b8cf8f56e041d35fd32ce6084ec3ea1c9cfc3
JulienOusmer <julienousmer46090@gmail.com>
2023-03-31 12:07:49 +0200

    fix link encore

--- include/footer.php
+++ include/footer.php
@@ -1,9 +1,9 @@
 <footer class="globalFooter py-3 my-4" id="bottom">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
         <li class="nav-item"><a href="../index.php" class="nav-link px-2 text-muted">Accueil</a></li>
-        <li class="nav-item"><a href="ConsultNews.php" class="nav-link px-2 text-muted">News</a></li>
-        <li class="nav-item"><a href="./views/ConsultEmplacement.php" class="nav-link px-2 text-muted">Recherches</a></li>
-        <li class="nav-item"><a href="./views/allEmplacements.php" class="nav-link px-2 text-muted">Consultation</a></li>
+        <li class="nav-item"><a href="./app/views/ConsultNews.php" class="nav-link px-2 text-muted">News</a></li>
+        <li class="nav-item"><a href="./app/views/ConsultEmplacement.php" class="nav-link px-2 text-muted">Recherches</a></li>
+        <li class="nav-item"><a href="./app/views/allEmplacements.php" class="nav-link px-2 text-muted">Consultation</a></li>
     </ul>
   <p class="text-center text-muted">© 2023 LP APSIO, Groupe 2</p>
 </footer>
