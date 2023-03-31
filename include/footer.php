0ea428bb334f831fa8243f93b35d8b8a2e5f04b8
JulienOusmer <julienousmer46090@gmail.com>
2023-03-31 12:01:59 +0200

    footer mieux

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,10 @@
-<footer class="globalFooter" id="bottom">
-  <p>LP APSIO - Groupe 2 ©</p>
-    <p><strong>Membres :</strong> Maxime, Titouan, Clément, Noémie, Thibaut, Julien</p>
+<footer class="globalFooter py-3 my-4" id="bottom">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted">Accueil</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">News</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Recherches</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Consultation</a></li>
+    </ul>
+  <p class="text-center text-muted">© 2023 LP APSIO, Groupe 2</p>
 </footer>
+
