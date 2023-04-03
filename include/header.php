49a296b35cda7a33e389b964db31107a02f7ea98
Borloo <supermaxime753@gmail.com>
2023-04-03 22:45:54 +0200

    fix DA

--- include/header.php
+++ include/header.php
@@ -1,10 +1,10 @@
 <header class="globalHeader" id="top">
     <div class="row">
         <div class="col-md-4"></div>
-        <div class="col-md-4">
+        <div class="col-md-5">
             <h1><a href="/index.php" class="text-decoration-none">Camping de la Grande Bleue</a></h1>
         </div>
-        <div class="col-md-2"></div>
+        <div class="col-md-1"></div>
         <div class="col-md-2">
             <a href="Deconnexion.php" class="text-decoration-none"><input class="btn btn-success" type="button" value="Déconnexion"></a>
         </div>
