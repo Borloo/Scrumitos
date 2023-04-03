7b5784c4346c59ff0b27ac548d8efa6ed0b600c7
Borloo <supermaxime753@gmail.com>
2023-04-04 01:59:19 +0200

    print

--- app/views/addAvis.php
+++ app/views/addAvis.php
@@ -38,8 +38,11 @@ require('./../base/HtmlFunctions.php');
     <section id="content">
             <?php
             if (isset($_GET['id'])){
+                print_r($_GET);
                 $location = getLocationById($_GET['id']);
+                print_r($location);
                 $emplacement = getEmplacementNameById($location['idEmplacement']);
+                print_r($emplacement);
                 echo "
                 <form method='post'>
                     <div class='card'>
