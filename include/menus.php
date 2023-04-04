68115876141f846265d51f06f9fd20022f6b113c
Borloo <supermaxime753@gmail.com>
2023-04-04 12:00:49 +0200

    suppr all emplacement from menu

--- include/menus.php
+++ include/menus.php
@@ -2,8 +2,7 @@
     <ul class="list-group">
         <li class="list-group-item"><a href="/index.php" class="text-decoration-none">Accueil</a></li>
         <li class="list-group-item"><a href="/app/views/ConsultNews.php" class="text-decoration-none">News</a></li>
-        <li class="list-group-item"><a href="/app/views/ConsultEmplacement.php" class="text-decoration-none">Recherches d'emplacements</a></li>
-        <li class="list-group-item"><a href="/app/views/allEmplacements.php" class="text-decoration-none">Consulter tous les emplacements</a></li>
+        <li class="list-group-item"><a href="/app/views/ConsultEmplacement.php" class="text-decoration-none">Emplacements</a></li>
         <?php
         // si l'admin ne s'est pas déja connecté alors on affiche le lien pour cela
         if (!isset($_SESSION['USER'])) {
