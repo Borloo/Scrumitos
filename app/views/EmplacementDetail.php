97a6490bb47fd218ac22adadeacec7304f2d4f5e
Borloo <supermaxime753@gmail.com>
2023-04-05 15:29:42 +0200

    test ?

--- app/views/EmplacementDetail.php
+++ app/views/EmplacementDetail.php
@@ -196,11 +196,7 @@ $emplacement = getOneEmplacementById((int)$_GET['id']);
                                         <div class='col-md-6'>
                                             <div class='input-group mb-3'>
                                                 <span class='input-group-text'>Image</span>
-                                                 <input class='form-control' name='image' type='file' value='" . $image . "'/>";
-                if ($_GET['edit'] == 0) {
-                    echo "readonly";
-                }
-                echo ">
+                                                 <input class='form-control' name='image' type='file' accept='image/*'>
                                             </div>
                                         </div>
                                     </div>
