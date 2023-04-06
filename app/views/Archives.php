717455b047e5d78912da924681eca449b19eb2d8
blbcrv <clementpages02@gmail.com>
2023-04-06 15:44:45 +0200

    oh oui

--- app/views/Archives.php
+++ app/views/Archives.php
@@ -25,7 +25,7 @@ require('./../base/HtmlFunctions.php');
                         <div class='col-md-5'></div>
                         <div class='col-md-2'>
                             <div class='input-group mb-3'>
-                                <span class='input-group-text' id='basic-addon2'>Année</span>
+                                <h1 class='input-group-text' id='basic-addon2'>Année</h1>
                                 <form method='POST'>
                                 <select class='form-select' required name='listYear'>";
                                 $years = getAnnee();
