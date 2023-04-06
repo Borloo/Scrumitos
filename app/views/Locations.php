03677ebda634efa088b6aac4efdd2ff369c4281e
Borloo <supermaxime753@gmail.com>
2023-04-06 14:20:56 +0200

    ok tier

--- app/views/Locations.php
+++ app/views/Locations.php
@@ -17,37 +17,6 @@ if (isset($_GET['user'])){
 <html>
 <head>
     <?php include("./../../include/headfile.php"); ?>
-    <style>
-        td {
-            text-align: center;
-            padding: 5px;
-        }
-
-        th {
-            padding: 5px 20px 5px 20px;
-        }
-
-        table {
-            border: 1px solid black;
-        }
-
-        select {
-            width: 100%;
-        }
-
-        .btn {
-            width: 100%;
-        }
-
-        .row {
-            margin: 2%;
-        }
-
-        .card {
-            margin-bottom: 2%;
-            height: 100%;
-        }
-    </style>
 </head>
 <body>
 <?php include("./../../include/header.php"); ?>
