9d16f00c4ea4dd80cf2993eb72ce2fb9b0e1b517
Noémie <rappeneau.noemie@gmail.com>
2023-04-06 16:09:18 +0200

    modif style

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,7 @@
 <footer class="globalFooter py-3 my-4" id="bottom">
     <p class="text-center text-muted"><strong>KekraTeam :</strong> Noémie - Titouan - Clément - Maxime - Thibaut - Julien</p>
   <p class="text-center text-muted">© 2023 LP APSIO, Groupe 2</p>
-    <img src="../../logoTeam.png" alt="logo">
+    <img id="imgKekra" src="../../logoTeam.png" alt="logo">
 </footer>
 
 <style>
