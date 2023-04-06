a6a4fe437b63a470eb7b43592b56d9df6aa3c526
JulienOusmer <julienousmer46090@gmail.com>
2023-04-06 16:09:16 +0200

    enlever marge footer

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,4 @@
-<footer class="globalFooter py-3 my-4" id="bottom">
+<footer class="globalFooter py-3" id="bottom">
     <p class="text-center text-muted"><strong>KekraTeam :</strong> Noémie - Titouan - Clément - Maxime - Thibaut - Julien</p>
   <p class="text-center text-muted">© 2023 LP APSIO, Groupe 2</p>
     <img src="../../logoTeam.png" alt="logo">
