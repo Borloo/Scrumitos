d399114a71f6054bd5630ae2c6b9adee844cebc0
JulienOusmer <julienousmer46090@gmail.com>
2023-04-06 08:18:23 +0200

    meilleur affichage

--- app/views/ConsultEmplacement.php
+++ app/views/ConsultEmplacement.php
@@ -113,7 +113,7 @@ require('./../base/HtmlFunctions.php');
             if (!empty($emplacements)) {
                 echo "
                     <table class='table'>
-                        <tr><th scope='col'>Nom</th><th scope='col'>Type</th><th scope='col'>Adresse</th><th scope='col'>Prix par semaine</th><th scope='col'>Aperçu</th></tr>";
+                        <tr><th scope='col'>Nom</th><th scope='col'>Type</th><th scope='col'>Adresse</th><th scope='col'>Prix par semaine</th><th scope='col'>Aperçu</th><th scope='col'>Actions</th></tr>";
                 getHtmlEmplacementTable($emplacements);
                 echo "</table>";
             } else {
