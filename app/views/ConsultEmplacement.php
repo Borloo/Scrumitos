5065488f667eefdc7c59fcdba3dabbf758705cab
Borloo <supermaxime753@gmail.com>
2023-04-06 15:12:10 +0200

    ok man

--- app/views/ConsultEmplacement.php
+++ app/views/ConsultEmplacement.php
@@ -9,6 +9,36 @@ require('./../base/HtmlFunctions.php');
 <html>
 <head>
     <?php include("./../../include/headfile.php"); ?>
+    <style>
+        td {
+            text-align: center;
+            padding: 5px;
+        }
+
+        th {
+            padding: 5px 20px 5px 20px;
+        }
+
+        table {
+            border: 1px solid black;
+        }
+
+        select {
+            width: 100%;
+        }
+
+        .btn {
+            width: 100%;
+        }
+
+        .row {
+            margin: 2%;
+        }
+
+        .card {
+            margin-bottom: 2%;
+        }
+    </style>
 </head>
 
 <body>
